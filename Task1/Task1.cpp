b14d41a587337afdcc4cdbfe74c5b6d23943af6c
R33my <reemysteam@gmail.com>
2022-11-12 10:27:08 +0200

    change

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <istream>
+#include <conio.h>
+#include <string>
+#include <sstream>
 
 //Lab 5. Variant 13. Task 1
 using namespace std;
 int main()
 {
-    cout << "Hello World!\n";
+	string str;
+	stringstream stream(str);
+	cout << "Type something: ";
+	getline(cin, str);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ':' && str[i - 1] != ':')
+		{
+			int temp = i + 1;
+			for (temp; temp < str.size(); temp++)
+			{
+				cout << str[temp];
+			}
+		}
+	}
+
+	while (cout << "Type something: " && getline(cin, str) && !str.empty()) {
+		int  i = 0, k = 0;
+		stringstream stream(str);
+		string tmp, t;
+		
+		cout << "Symbols after ':' - " << endl;
+		if (t[i] == ':' && t[i - 1] != ':')
+		{
+			for (i; i < str.size(); i++)
+			{
+				cout << t[i];
+			}
+		}
+		i++;
+	}
+	return 0;
 }
